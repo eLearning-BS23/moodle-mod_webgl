cb42a59415d3cf5cafb313efd04bbe5dcc371d27
sahedbs23 <ssawon.bs23@gmail.com>
2020-11-27 19:04:58 +0600

    course module view event unwanted code removed. module view code clean up

--- classes/event/course_module_viewed.php
+++ classes/event/course_module_viewed.php
@@ -24,7 +24,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace mod_appstream\event;
+namespace mod_webgl\event;
 
 defined('MOODLE_INTERNAL') || die();
 
