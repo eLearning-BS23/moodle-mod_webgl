138431ae0e62d18d1b4dbb69d836b19af3fac160
Shadman <shadman.chowdhury@brainstation23.com>
2021-08-17 14:08:34 +0600

    Remove unused commented out code

--- lib.php
+++ lib.php
@@ -155,12 +155,6 @@ function webgl_refresh_events($courseid = 0): bool {
             return true;
         }
     }
-    /*
-     foreach ($webgls as $webgl) {
-         Create a function such as the one below to deal with updating calendar events.
-         webgl_update_events($webgl);
-     }
-    */
 
     return true;
 }
@@ -191,7 +185,6 @@ function webgl_delete_instance($id): bool {
     } else {
         delete_container_blobs($webgl);
     }
-    /* webgl_grade_item_delete($webgl); */
 
     return true;
 }
