22ab1a266e38973400749772743d146b9893c092
Shadman <shadman.chowdhury@brainstation23.com>
2021-09-06 16:42:58 +0600

    Update version of the plugin

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021081701; // The current version (Date: YYYYMMDDXX).
+$plugin->version = 2021090601; // The current version (Date: YYYYMMDDXX).
 
 $plugin->requires = 2019111806; // Requires this Moodle version.
 
